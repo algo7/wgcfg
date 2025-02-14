cd3c79263fc77e9901bfb302a674d8707b20161b
algo7 <11154774+algo7@users.noreply.github.com>
2025-02-14 10:15:45 +0800

    implement main function to generate and display WireGuard configuration

--- main.go
+++ main.go
@@ -1,3 +1,56 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net"
+
+	"github.com/algo7/wgcfg/internal/config"
+	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
+)
+
+func main() {
+
+	privateKey, err := wgtypes.GeneratePrivateKey()
+	if err != nil {
+		panic(err)
+	}
+
+	publicKey := privateKey.PublicKey()
+
+	config := config.Config{
+		Interface: config.Interface{
+			Address: net.IPNet{
+				IP:   net.ParseIP("12.12.12.12"),
+				Mask: net.CIDRMask(24, 32),
+			},
+			PrivateKey: privateKey,
+			DNS: []net.IP{
+				net.ParseIP("12.12.123.12"),
+			},
+			MTU: 1420,
+		},
+		Peers: []config.Peer{
+			{
+				Endpoint: &net.UDPAddr{
+					IP:   net.ParseIP("12.12.12.12"),
+					Port: 51820,
+				},
+				AllowedIPs: []net.IPNet{
+					{
+						IP:   net.ParseIP("12.12.32.12"),
+						Mask: net.CIDRMask(24, 32),
+					},
+				},
+				PublicKey: publicKey,
+			},
+		},
+	}
+
+	cfg, err := config.Generate()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(cfg)
+
+}
